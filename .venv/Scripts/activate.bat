5a685669d9c12edaf7375dbdbaae2e2324c8474f
Martin  Stoyanov <martotbb.2556@gmail.com>
2025-11-30 13:07:08 +0200

    environment fix

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\FMI\SPA\python\.venv
+set VIRTUAL_ENV=D:\FMI\SPA\scripts\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ scatter-plots.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import camelot as cmt
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
